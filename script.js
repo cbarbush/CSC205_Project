9fcd005c4ff45dfca286632e477142322c5c7cc0
cbarbush <cdbar03@gmail.com>
2023-10-04 20:00:45 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+        
+gsap.from(".first", {opacity: 0, duration: 1, delay: .6})
+gsap.from(".second", {opacity: 0, duration: 1, delay: 1})
+gsap.from(".third", {opacity: 0, duration: 1, delay: 1.4})
+
+gsap.from(".navlist", {duration: 1, y: -200, delay: 0.6})
+gsap.from(".header", {duration: .6, y: -200})
