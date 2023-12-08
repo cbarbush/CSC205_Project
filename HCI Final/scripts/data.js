6449ab336ec6b27468674f3f78a598882e754560
cbarbush <cdbar03@gmail.com>
2023-12-07 19:09:25 -0500

    Components Part 2

--- HCI Final/scripts/data.js
+++ HCI Final/scripts/data.js
@@ -1,209 +1,384 @@
-const options1 = {
-    chart: {
-        height: 450,
-        width: '1000px',
-        height: '150px',
-        type: 'heatmap',
-        background: '#f4f4f4',
-        foreColor: '#333',
-        toolbar: {
-            show: false
-          },
-          zoom: {
-            enabled: false,
-          }
-    },
-    
-    series: [
-        {
-            name: "KC28",
-            data: [3,4,5,1,1,3,4,5,3,3,4,4,4,3,2,5,1,5,5,2]
-          }
-    ]
+const questionUrl = "http://football.cscprof.com/questions"
+var coachID = parseInt(sessionStorage.getItem("coachID"))
+var answerUrl = `http://football.cscprof.com/coaches/${coachID}/answers`
+var averages = [];
+var coachData = [];
+var chartHeight;
+const preloader = document.querySelector(".preloader")
+const coachUrl = "http://football.cscprof.com/coaches";
+var headCoaches = [];
+
+// function to enable drop down box
+function enableDropDown() {
+  if ((window.location.pathname.includes('team.html') || window.location.pathname.includes('assistant.html')) && sessionStorage.getItem("accessLevel") == 3) {
+    var dropDown = document.querySelector('.select-container')
+    dropDown.classList.remove('disabled')
+  }
 }
 
-const options2 = {
-    chart: {
-        height: 450,
-        width: '1000px',
-        height: '150px',
-        type: 'heatmap',
-        background: '#f4f4f4',
-        foreColor: '#333',
-        toolbar: {
-            show: false
-          },
-          zoom: {
-            enabled: false,
-          }
-    },
-    
-    series: [
-        {
-            name: "EJ34",
-            data: [2, 4, 1, 5, 3, 1, 4, 2, 5, 3, 2, 4, 1, 3, 5, 1, 3, 4, 2, 5]
-          }
-    ]
+enableDropDown()
+
+async function getQuestions() {
+    const response = await fetch(questionUrl); // fetch questions
+    return await response.json();
 }
 
-const options3 = {
-    chart: {
-        height: 450,
-        width: '1000px',
-        height: '150px',
-        type: 'heatmap',
-        background: '#f4f4f4',
-        foreColor: '#333',
-        toolbar: {
-            show: false
-          },
-          zoom: {
-            enabled: false,
-          }
-    },
-    
-    series: [
-        {
-            name: "JD20",
-            data: [5, 2, 1, 4, 3, 1, 3, 2, 4, 5, 2, 1, 4, 3, 5, 1, 4, 2, 3, 5]
-          }
-    ]
+getQuestions();
+
+// function to update the tooltip on the chart
+function updateTooltip(seriesIndex, dataPointIndex, questions) {
+  const question = questions[dataPointIndex]; // get questions
+  const questionNumber = dataPointIndex + 1;
+  // return an element with the question number and question
+  return `<span class="tt">${questionNumber}. ${question ? question.Question : 'No question found'}</span>`;
 }
 
-const teamData = {
-    chart: {
-        height: 450,
-        width: '1000px',
-        height: '150px',
-        type: 'heatmap',
-        background: '#f4f4f4',
-        foreColor: '#333',
-        toolbar: {
-            show: false
-          },
-          zoom: {
-            enabled: false,
-          }
-    },
-    
-    series: [
-        {
-            name: "Eagles",
-            data: [4.2, 4.6, 3.1, 2.2, 4.9, 1.8, 3.8, 2.1, 4.2, 3.4, 2.2, 1.7, 4.5, 3.4, 4.9, 1.2, 4.1, 2.3, 3.8, 4.6]
-          }
-    ]
+// function to run when team is selected from drop down box
+function selectTeam() {
+  var teamSelect = document.getElementById("team");
+  selectedIndex = teamSelect.selectedIndex; // get index from drop down box
+  const headCoachIDs = Object.keys(coachCount); // create array of head coach IDs
+  coachID = parseInt(headCoachIDs[selectedIndex]);
+  console.log("Head Coach ID from array: " + coachID)
 }
 
-const leagueData = {
-    chart: {
-        height: 450,
-        width: '1000px',
-        height: '150px',
-        type: 'heatmap',
-        background: '#f4f4f4',
-        foreColor: '#333',
-        toolbar: {
-            show: false
-          },
-          zoom: {
-            enabled: false,
-          }
-    },
-    
-    series: [
-        {
-            name: "League Average",
-            data: [4.7, 2, 1, 3.2, 3, 1, 3.8, 2, 4.5, 5, 2, 1.4, 4, 3.6, 5, 1, 4.2, 2, 3.9, 5]
-          }
-    ]
+
+const teamElement = document.getElementById("team");
+
+// check that teamElement exists
+if (teamElement) {
+  teamElement.addEventListener("change", function () {
+    console.log("Selection changed.")
+    const accessLevel = parseInt(sessionStorage.getItem("accessLevel"));
+
+    // if statement for assistant page
+    if (accessLevel === 3 && window.location.pathname.includes('assistant.html')) {
+      selectTeam();
+      renderCharts();
+    }
+
+    // if statement for team page
+    if (accessLevel === 3 && window.location.pathname.includes('team.html')) {
+      var teamSelect = document.getElementById("team");
+      selectedIndex = teamSelect.selectedIndex;
+      teamIdArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+      currentTeamId = teamIdArr[selectedIndex];
+      renderCharts();
+    }
+  });
 }
 
-const ageData1 = {
-    chart: {
-        height: 450,
-        width: '1000px',
-        height: '150px',
-        type: 'heatmap',
-        background: '#f4f4f4',
-        foreColor: '#333',
-        toolbar: {
-            show: false
-          },
-          zoom: {
-            enabled: false,
-          }
-    },
-    
-    series: [
-        {
-            name: "6/7",
-            data: [2.5, 4, 1, 3, 3.2, 1, 3.7, 2, 4.1, 5, 2, 1.3, 4, 3.9, 5, 1, 4.6, 2, 3.4, 5]
-          }
-    ]
+const coachCount = {}; // create empty dictionary
+
+async function getAssistantNumber() {
+  const teamUrl = "http://football.cscprof.com/teams";
+  const response = await fetch(teamUrl); // fetch data
+  const data = await response.json(); 
+
+  // loop through each team and add head coach IDs as keys and the number of assistants as values
+  data.forEach(team => {
+    const headCoach = team.coaches.find(coach => coach.AccessLevel === 2);
+
+    if (headCoach) {
+      const headCoachID = headCoach.coachID;
+
+      const assistantCount = team.coaches.reduce((count, coach) => {
+        return count + (coach.AccessLevel === 1 ? 1 : 0);
+      }, 0);
+
+      coachCount[headCoachID] = assistantCount;
+    }
+  });
+  console.log(coachCount);
 }
 
-const ageData2 = {
-    chart: {
-        height: 450,
-        width: '1000px',
-        height: '150px',
-        type: 'heatmap',
-        background: '#f4f4f4',
-        foreColor: '#333',
-        toolbar: {
-            show: false
-          },
-          zoom: {
-            enabled: false,
-          }
-    },
-    
-    series: [
-        {
-            name: "7/8",
-            data: [5, 2, 1.8, 4.3, 3, 1.5, 3.2, 2.7, 4, 5, 2, 1.2, 4.1, 3, 5, 1.6, 4, 2.9, 3.4, 5]
-          }
-    ]
+// function to create an array with all head coach IDs
+async function getHeadCoaches() {
+  const response = await fetch(coachUrl);
+  const coaches = await response.json();
+
+  for (const coach of coaches) {
+    if (coach.AccessLevel == 2){
+      headCoaches.push(coach.coachID);
+    }
+  }
+  return headCoaches;
 }
 
-const ageData3 = {
-    chart: {
-        height: 450,
-        width: '1000px',
-        height: '150px',
-        type: 'heatmap',
-        background: '#f4f4f4',
-        foreColor: '#333',
-        toolbar: {
-            show: false
-          },
-          zoom: {
-            enabled: false,
+
+async function getHeadCoachData() {
+  headCoArr = await getHeadCoaches() // get head coach IDs from getHeadCoaches
+    console.log(headCoArr)
+      for (const currentCoachID of headCoaches) {
+        
+        const currentAnswerUrl = `http://football.cscprof.com/coaches/${currentCoachID}/answers`;
+        const response = await fetch(currentAnswerUrl);
+        const data = await response.json();
+  
+        console.log(currentCoachID)
+  
+        // validate that data exists and is in the correct format
+        if (data && Array.isArray(data.answers)) {
+          const answers = data.answers;
+          const coachName = data.Name;
+  
+          const coachAverages = [];
+  
+          // loop through each question and get the average answer for each
+          for (let questionID = 2; questionID <= 19; questionID++) {
+            let sum = 0;
+            let count = 0;
+  
+            for (const answer of answers) {
+              if (answer.questionID === questionID && answer.Answer !== null) {
+                sum += parseInt(answer.Answer);
+                count++;
+              }
+            }
+  
+            const average = count > 0 ? (sum / count).toFixed(1) : null;
+            coachAverages.push(average);
           }
-    },
-    
-    series: [
-        {
-            name: "8/9",
-            data: [4.2, 2.8, 1, 3, 3.5, 1.2, 3.7, 2, 4.6, 5, 2.1, 1, 4, 3.3, 5, 1.4, 4.9, 2, 3, 5]
-          }
-    ]
+  
+          // push average and coach name to coachData object
+          coachData.push({
+            name: coachName,
+            data: coachAverages,
+          });
+          
+        } else {
+          console.error('Unexpected or missing data format for coachID:', currentCoachID);
+        }
+      }
+      return coachData;
 }
 
-const chart1 = new ApexCharts(document.querySelector('#chart1'), options1);
-const chart2 = new ApexCharts(document.querySelector('#chart2'), options2);
-const chart3 = new ApexCharts(document.querySelector('#chart3'), options3);
-const teamChart = new ApexCharts(document.querySelector('#teamChart'), teamData)
-const leagueChart = new ApexCharts(document.querySelector('#leagueChart'), leagueData)
-const ageChart1 = new ApexCharts(document.querySelector('#ageChart1'), ageData1);
-const ageChart2 = new ApexCharts(document.querySelector('#ageChart2'), ageData2);
-const ageChart3 = new ApexCharts(document.querySelector('#ageChart3'), ageData3);
 
-chart1.render();
-chart2.render();
-chart3.render();
-teamChart.render();
-leagueChart.render();
-ageChart1.render();
-ageChart2.render();
-ageChart3.render();
+async function getData() {
+  averages = [];
+  const accessLevel = parseInt(sessionStorage.getItem("accessLevel")); // get access level from session storage
+  let iterator, startingCoachID, endingCoachID;
+  let checkHead = false;
+  
+
+  // special case for head coach on head coach page
+  if (accessLevel == 2 && window.location.pathname.includes('head.html')) {
+    checkHead = true;
+  }
+
+  if (accessLevel > 1 && !checkHead) {
+    chartHeight = 'auto';
+    
+    if (window.location.pathname.includes('assistant.html')) {
+      await getAssistantNumber(); // get number of assistant coaches from function
+      if (accessLevel == 3) {
+        selectTeam(); // handle the drop down menu for coordinator
+      }
+
+      // set values for for loop
+      startingCoachID = coachID + 1;
+      iterator = 1;
+      endingCoachID = startingCoachID + coachCount[coachID] - 1;
+    }
+
+    coachData = [];
+
+    // special case for team page
+    if (window.location.pathname.includes('team.html')) {
+      chartHeight = '150'
+      coachData = await getTeamData()
+    }
+
+    // special case for league page
+    if (window.location.pathname.includes('league.html')) {
+      coachData = await getAllAverages();
+    }
+
+    // special case for head coach page as coordinator
+    if (window.location.pathname.includes('head.html') && accessLevel == 3) {
+      coachData = await getHeadCoachData();
+    }
+
+
+
+
+    // loop through each coach
+    for (let currentCoachID = startingCoachID; currentCoachID <= endingCoachID; currentCoachID += iterator) {
+      const currentAnswerUrl = `http://football.cscprof.com/coaches/${currentCoachID}/answers`; // send a request for each coach in the loop
+      const response = await fetch(currentAnswerUrl); // fetch data
+      const data = await response.json();
+
+      console.log(currentCoachID)
+
+      // validate that data exists and is in the correct format
+      if (data && Array.isArray(data.answers)) {
+        const answers = data.answers;
+        const coachName = data.Name;
+
+        const coachAverages = [];
+
+        // get average for each question and push to an array
+        for (let questionID = 2; questionID <= 19; questionID++) {
+          let sum = 0;
+          let count = 0;
+
+          for (const answer of answers) {
+            if (answer.questionID === questionID && answer.Answer !== null) {
+              sum += parseInt(answer.Answer);
+              count++;
+            }
+          }
+
+          const average = count > 0 ? (sum / count).toFixed(1) : null;
+          coachAverages.push(average);
+        }
+
+        // push average and coach name to coachData object
+        coachData.push({
+          name: coachName,
+          data: coachAverages,
+        });
+        
+      } else {
+        console.error('Unexpected or missing data format for coachID:', currentCoachID);
+      }
+    }
+  } 
+
+  // case to handle individual coach
+  else {
+    chartHeight = '150'
+    const response = await fetch(answerUrl);
+    const data = await response.json();
+    
+    // validate that data exists and is in the correct format
+    if (data && Array.isArray(data.answers)) {
+      const answers = data.answers;
+
+      // loop through each question and calculate average answer
+      for (let questionID = 2; questionID <= 19; questionID++) {
+        let sum = 0;
+        let count = 0;
+
+        for (const answer of answers) {
+          if (answer.questionID === questionID && answer.Answer !== null) {
+            sum += parseInt(answer.Answer);
+            count++;
+          }
+        }
+
+        const average = count > 0 ? (sum / count).toFixed(1) : null;
+        averages.push(average); // push average to array
+      }
+
+      // push average and coach name to coachData object
+      coachData.push({
+        name: sessionStorage.getItem("coachName"),
+        data: averages
+      })
+    } 
+    else {
+      console.error('Unexpected or missing data format. Please check the API response structure.');
+    }
+  }
+}
+
+
+
+
+
+
+
+
+let chart1;
+
+async function renderCharts() {
+  await getData(); // get the coachData from the function
+  const questions = await getQuestions(); // get questions for tooltip
+
+  // chart options
+  const options1 = {
+      chart: {
+          width: '1000px',
+          height: chartHeight,
+          type: 'heatmap',
+          background: '#f4f4f4',
+          foreColor: '#333',
+          toolbar: {
+              show: false
+            },
+            zoom: {
+              enabled: false,
+            }
+      },
+      
+      plotOptions: {
+        heatmap: {
+          shadeIntensity: 0.5,
+          radius: 0,
+          useFillColorAsStroke: false,
+          colorScale: {
+            ranges: [{
+                from: 0,
+                to: 0.9,
+                name: 'Poor',
+                color: '#b3cde0'
+              },
+              {
+                from: 1.0,
+                to: 2.0,
+                name: 'Below Average',
+                color: '#6497b1'
+              },
+              {
+                from: 2.1,
+                to: 3.0,
+                name: 'Fair',
+                color: '#005b96'
+              },
+              {
+                from: 3.1,
+                to: 4.0,
+                name: 'Good',
+                color: '#03396c'
+              },
+              {
+                from: 4.1,
+                to: 5.0,
+                name: 'Excellent',
+                color: '#011f4b'
+              }
+            ]
+          }
+        }
+      },
+      
+      series: coachData,
+      colors: ["#008FFB"],
+      tooltip: {
+        custom: function ({ seriesIndex, dataPointIndex, w }) {
+          return updateTooltip(seriesIndex, dataPointIndex, questions); // run updateTooltip
+        }
+      },
+      legend: {
+        fontSize: '12px',
+        itemMargin: {
+          horizontal: 5,
+          vertical: 20
+        }
+      }
+  }
+
+  // check if chart exists
+  if (!chart1) {
+    chart1 = new ApexCharts(document.querySelector('#chart1'), options1);
+    chart1.render();
+    preloader.classList.add("disabled") // disable the spinner
+  } 
+  // if the chart exists, update the data instead of rendering a new one
+  else {
+    chart1.updateSeries(coachData, true);
+  }
+}
+
+renderCharts()
--- HCI Final/scripts/script.js
+++ HCI Final/scripts/script.js
@@ -13,6 +13,9 @@ const access1 = document.querySelector(".access1")
 const access2 = document.querySelectorAll(".access2")
 const access3 = document.querySelector(".access3")
 
+let coaches;
+
+// add disabled class to navbar items
 function removeAccess() {
     access1.classList.add("disabled");
     access2.forEach(element => {
@@ -22,6 +25,8 @@ function removeAccess() {
     headcoachLI.innerHTML = "Head Coaches";
     assistantLI.innerHTML = "Assistant Coaches";
 }
+
+// get access level and adjust navbar accordingly
 function addAccess() {
     if (sessionStorage.getItem("accessLevel") === "3") {
         access1.classList.remove("disabled");
@@ -42,19 +47,17 @@ function addAccess() {
         access1.classList.remove("disabled");
     }
 }
-// document.addEventListener('DOMContentLoaded', function() {
-//     if (sessionStorage.set)
-// });
 
+// add event listener for login button on page load
 document.addEventListener('DOMContentLoaded', function () {
     loginButton.addEventListener('click', (event) => {
         if (sessionStorage.getItem("loggedin") !== "true") {
-            event.preventDefault();
-            wrapper.classList.add('active-popup');
-            page.classList.add('no-scroll')
+            event.preventDefault(); // prevent refresh on button click
+            wrapper.classList.add('active-popup'); // make login form appear
+            page.classList.add('no-scroll') // disable ability to scroll
         }
         else {
-            sessionStorage.clear();
+            sessionStorage.clear(); // clear session storage
             btn.innerHTML= "Login"
             console.log("Success")
             usernameInput.value = "";
@@ -63,6 +66,8 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     
     });
+
+    // event listener for the close icon on login form
     iconClose.addEventListener('click', ()=> {
         wrapper.classList.remove('active-popup')
         page.classList.remove('no-scroll')
@@ -74,22 +79,9 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log(sessionStorage.getItem("accessLevel"));
 });
 
-
+// event listener for button on login form
 mainLoginButton.addEventListener('click', function() {
-    if (passwordInput.value !== "" && usernameInput.value !== "") {
-        inputtedUsername = usernameInput.value;
-        const foundCoach = coaches.find(coach => coach.userID === inputtedUsername);
-
-        if (foundCoach) {
-            handleLogin(foundCoach)
-        }
-        else {
-            alert("Username not found.")
-        }
-    }
-    else {
-        console.log("no username or passoword")
-    }
+    validateInput()
 });
 
 
@@ -104,16 +96,42 @@ mainLoginButton.addEventListener('click', function() {
 
 
 
+function validateInput() {
+    // make sure inputs are not empty
+    if (passwordInput.value !== "" && usernameInput.value !== "") {
+        inputtedUsername = usernameInput.value;
+        // compare input to coach data from database
+        const foundCoach = coaches.find(coach => coach.userID === inputtedUsername);
 
+        if (foundCoach) {
+            handleLogin(foundCoach)
+        }
+        else {
+            alert("Username not found.")
+        }
+    }
+    else {
+        console.log("no username or password")
+    }
+}
 
 
 
 
 function handleLogin(foundCoach) {
     const accessLevel = foundCoach.AccessLevel;
+    const coachID = foundCoach.coachID;
+    const teamID = foundCoach.TeamId;
+    const coachName = foundCoach.Name
+
+    // set session variables
     sessionStorage.setItem('accessLevel', accessLevel);
-    sessionStorage.setItem('loggedin', true)
-    wrapper.classList.remove('active-popup')
+    sessionStorage.setItem('loggedin', true);
+    sessionStorage.setItem('coachID', coachID);
+    sessionStorage.setItem('teamID', teamID);
+    sessionStorage.setItem('coachName', coachName)
+
+    wrapper.classList.remove('active-popup') // close login form
     page.classList.remove('no-scroll')
 
     addAccess()
@@ -121,30 +139,14 @@ function handleLogin(foundCoach) {
 }
 
 
-const coaches = [{
-    "coachID": 2,
-    "userID": "FC2",
-    "Name": " Football Coordinator",
-    "AccessLevel": 3,
-    "Password": "f00tba!!",
-    "TeamId": 0,
-    "EmailAddress": "xyz@ggmail.ocm"
-  },
-  {
-    "coachID": 27,
-    "userID": "JG27",
-    "Name": " Jospeh Gillespie",
-    "AccessLevel": 2,
-    "Password": "f00tba!!",
-    "TeamId": 1,
-    "EmailAddress": "xyz@ggmail.ocm"
-  },
-  {
-    "coachID": 28,
-    "userID": "KC28",
-    "Name": " Kenton Chaney",
-    "AccessLevel": 1,
-    "Password": "f00tba!!",
-    "TeamId": 1,
-    "EmailAddress": "xyz@ggmail.ocm"
-  }]
+
+const coachUrl = "http://football.cscprof.com/coaches"
+
+async function getCoaches() {
+    const response = await fetch(coachUrl); // fetch coach data
+    coaches = await response.json();
+    console.log(coaches)
+}
+
+
+getCoaches()
